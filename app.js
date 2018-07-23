15a01693f44375129d94a7303c3eb68e7028b8e1
Sibi Sharanyan <sibi309@gmail.com>
2018-07-23 17:16:47 +0000

    Added local authentication

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express     = require("express"),
     app         = express(),
-    bodyParser = require("body-parser"),
+    bodyParser  = require("body-parser"),
     stringSimilarity = require('string-similarity'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    user = require("./models/user");
 
 mongoose.connect('mongodb://localhost:27017/spamDB'  , { useNewUrlParser: true });
 var schema = new mongoose.Schema({ message: 'string' , reason: 'string'  });
@@ -14,20 +18,82 @@ let max;
 let si;
 let matchedSpam;
 let re;
+app.use(require("express-session")({
+    secret : "Sibi the great" ,
+    resave : false ,
+    saveUninitialized : false
+    
+}));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
+passport.use(new LocalStrategy(user.authenticate()));   
+
+function isLoggedIn(req , res , next )
+{
+    if(req.isAuthenticated())
+    {
+        return next();
+    }
+    res.redirect("/login");
+}
+
+
     
 app.get("/" , function(req,res){
     res.redirect("/index");
 })
 
+app.get("/register" , function(req,res){
+    res.render("register");
+})
+app.post("/register" , function(req,res){
+        req.body.username
+        req.body.password
+        user.register(new user({username : req.body.username }) , req.body.password , function(err, user){
+            if(err)
+            {
+                console.log(err);
+                return res.render('register');
+                
+            }
+            else{
+                passport.authenticate("local")(req , res , function(){
+                   res.redirect("/submit") ;
+                });
+            }
+        })
+})
+
+
+app.get("/login" , function(req,res){
+    res.render("login");
+})
+
+app.post("/login" , passport.authenticate("local" ,    { successRedirect : "/submit" , 
+                                                        failureRedirect : "/login"
+}) , function(req,res){
+    
+})
+
+app.get("/logout" , function(req,res){
+   req.logout();
+   res.redirect("/");
+   
+})
+
+
 app.get("/index" , function(req,res){
     res.render("front.ejs");
 })
 
-app.get("/submit" , function(req,res){
+app.get("/submit" , isLoggedIn ,  function(req,res){
     res.render("submit.ejs");
 })
 
